d3df031414a9b1b491374d2f822a95a18a581d8c
Mehran Hosseini <ajaxcube@gmail.com>
2020-02-24 03:11:00 +0100

    Creating different beans to get injected for everyone of Components with @Qualifier("") to tell which of controllers get which one of services

---
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World- Setter";
+    }
+}
